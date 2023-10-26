dfa305e63986209428ac1333e2f0fa8076fcea3f
romeo <rjameson1928@gmail.com>
2023-10-27 00:28:21 +0300

    update Clock component

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -27,25 +27,41 @@ export const Clock = () => {
 		};
 	}
 
+	// style
+	const hourStyle = {
+		transform: `translateX(-50%) translateY(-100%) rotate(${(currentTime.hours % 12) * 30 + currentTime.minutes / 2}deg)`
+	}
+
+	const minuteStyle = {
+		transform: `translateX(-50%) translateY(-100%) rotate(${currentTime.minutes * 6}deg)`
+	}
+
+	const secondStyle = {
+		transform: `translateX(-50%) translateY(-100%) rotate(${currentTime.seconds * 6}deg)`
+	}
+
 	return (
 		<div className="clock">
 			<div
 				className="hourHand"
-				style={{
-					transform: `translateX(-50%) translateY(-100%) rotate(${(currentTime.hours % 12) * 30 + currentTime.minutes / 2}deg)`,
-				}}
+				style={hourStyle}
+				// style={{
+				// 	transform: `translateX(-50%) translateY(-100%) rotate(${(currentTime.hours % 12) * 30 + currentTime.minutes / 2}deg)`,
+				// }}
 			></div>
 			<div
 				className="minuteHand"
-				style={{
-					transform: `translateX(-50%) translateY(-100%) rotate(${currentTime.minutes * 6}deg)`,
-				}}
+				style={minuteStyle}
+				// style={{
+				// 	transform: `translateX(-50%) translateY(-100%) rotate(${currentTime.minutes * 6}deg)`,
+				// }}
 			></div>
 			<div
 				className="secondHand"
-				style={{
-					transform: `translateX(-50%) translateY(-100%) rotate(${currentTime.seconds * 6}deg)`,
-				}}
+				style={secondStyle}
+				// style={{
+				// 	transform: `translateX(-50%) translateY(-100%) rotate(${currentTime.seconds * 6}deg)`,
+				// }}
 			></div>
 		</div>
 	);
